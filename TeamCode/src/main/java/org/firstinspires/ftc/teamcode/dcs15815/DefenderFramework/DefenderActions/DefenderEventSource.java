1a70353df4f42c69bdc0bf31614bbf4d1a366eca
Matthew Roberts <thew.roberts@gmail.com>
2023-10-20 10:59:25 -0400

    Removed Framework code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/DefenderFramework/DefenderActions/DefenderEventSource.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.dcs15815.DefenderFramework.DefenderActions;
-
-import java.util.ArrayList;
-
-public interface DefenderEventSource {
-
-    public ArrayList<DefenderEvent> gatherEvents();
-}

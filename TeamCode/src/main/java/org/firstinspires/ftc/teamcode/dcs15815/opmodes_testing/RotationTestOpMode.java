c1a4a28951aee03e5e6a038174f328d93927056a
Matthew Roberts <thew.roberts@gmail.com>
2023-11-01 10:49:18 -0400

    Added new testing opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/RotationTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/RotationTestOpMode.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.dcs15815.StickyBanditBot.SBBConfiguration;
 import org.firstinspires.ftc.teamcode.dcs15815.StickyBanditBot.StickyBanditBot;
 
-@TeleOp(name = "Rottion Test", group = "Testing")
+@TeleOp(name = "Rotation Test", group = "Testing")
 public class RotationTestOpMode extends LinearOpMode {
     StickyBanditBot bot;
 
@@ -14,9 +14,10 @@ public class RotationTestOpMode extends LinearOpMode {
 	   bot = new StickyBanditBot(hardwareMap, SBBConfiguration.class, telemetry);
 
 	   waitForStart();
-
+		double d = 0;
 	   while (opModeIsActive()) {
-
+		d += 90;
+		bot.navigation.comeToHeading(d, 0.25, 5, 10000);
 	   }
     }
 }

3c9f58a29d7ac9e28b45977c7989452fae4ea2ce
Matthew Roberts <thew.roberts@gmail.com>
2023-11-02 16:57:59 -0400

    Declared final after testing on 11/02/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes/AutonomousFar1OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes/AutonomousFar1OpMode.java
@@ -17,50 +17,77 @@ public class AutonomousFar1OpMode extends PropDetectingOpMode {
 
     @Override
     public void whenRedAlliance() {
-	   if (position == PropVisionProcessor.PropPosition.MIDDLE) {
-		  bot.navigation.driveToPosition(0, -52, 0, 0.1);
-		  bot.stickyPad.releaseRight();
-		  sleep(2000);
-
-		  // Move to center field and turn towards backstage
-		  bot.gotoTravelArmPosition();
-		  bot.navigation.resetAndDriveToPosition(0, -15, 0, 0.1);
-		  bot.gotoStartArmPosition();
-		  bot.navigation.comeToRelativeHeading(90, 0.3, 1, 5000);
-
-
-	   } else if (position == PropVisionProcessor.PropPosition.LEFT) {
-		  bot.drivetrain.drive(0, -0.25, 0);
-		  sleep(460);
-		  bot.navigation.resetAndDriveToPosition(0, -47, 0, 0.1);
-		  bot.stickyPad.releaseRight();
-		  sleep(2000);
-
-		  // Move to center field and turn towards backstage
-		  bot.gotoTravelArmPosition();
-		  bot.navigation.resetAndDriveToPosition(0, -15, 0, 0.1);
-		  bot.gotoStartArmPosition();
-		  bot.navigation.comeToRelativeHeading(90, 0.3, 1, 5000);
-
-
-	   } else if (position == PropVisionProcessor.PropPosition.RIGHT) {
-		  bot.navigation.driveToPosition(0, -32, 0, 0.1);
-		  bot.navigation.comeToRelativeHeading(90, 0.3, 1, 4000);
-		  bot.gotoTravelArmPosition();
-		  sleep(1500);
-		  bot.stickyPad.releaseRight();
-		  sleep(2000);
-
-		  // Move to center field and turn towards backstage
-		  bot.navigation.resetAndDriveToPosition(-15, 0, 0, 0.1);
-		  bot.gotoStartArmPosition();
-
-	   }
 
     }
 
     @Override
     public void whenBlueAlliance() {
+	   if (position == PropVisionProcessor.PropPosition.MIDDLE) {
+		  bot.navigation.driveToPosition(0, -52, 0, 0.1);
+		  bot.stickyPad.releaseLeft();
+		  sleep(2000);
 
+		  // Move to center field and turn towards backstage
+		  bot.gotoTravelArmPosition();
+		  bot.navigation.resetAndDriveToPosition(0, -15, 0, 0.1);
+		  bot.navigation.comeToRelativeHeading(-90, 0.3, 1, 5000);
+
+		  // Drive across field
+		  bot.navigation.resetAndDriveToPosition(0, 86, 0, 0.1);
+		  bot.navigation.resetAndDriveToPosition(-25, 0, 0, 0.35);
+		  bot.navigation.resetAndDriveToPosition(0, 7, 0, 0.2);
+
+		  bot.stickyPad.releaseRight();
+		  sleep(2000);
+
+		  bot.gotoStartArmPosition();
+		  sleep(2000);
+
+
+
+	   } else if (position == PropVisionProcessor.PropPosition.LEFT) {
+
+		  bot.navigation.resetAndDriveToPosition(0, -33, 0, 0.1);
+		  bot.navigation.comeToRelativeHeading(90, 0.3, 1, 4000);
+		  bot.navigation.resetAndDriveToPosition(0, -28, 0, 0.1);
+		  bot.gotoTravelArmPosition();
+		  sleep(1500);
+		  bot.stickyPad.releaseLeft();
+		  sleep(2000);
+
+		  bot.navigation.resetAndDriveToPosition(0, -75, 0, 0.15);
+		  bot.navigation.resetAndDriveToPosition(-18, 0, 0, 0.5);
+		  bot.gotoHighBackDeliveryPosition();
+		  sleep(2500);
+		  bot.stickyPad.releaseRight();
+		  sleep(2000);
+		  bot.gotoStartArmPosition();
+		  sleep(2000);
+
+	   } else if (position == PropVisionProcessor.PropPosition.RIGHT) {
+		  bot.drivetrain.drive(0, -0.25, 0);
+		  sleep(460);
+		  bot.navigation.resetAndDriveToPosition(0, -47, 0, 0.1);
+		  bot.stickyPad.releaseLeft();
+		  sleep(2000);
+
+		  // Move to center field and turn towards backstage
+		  bot.gotoTravelArmPosition();
+		  bot.navigation.resetAndDriveToPosition(0, -20, 0, 0.1);
+		  bot.navigation.comeToRelativeHeading(-95, 0.3, 1, 5000);
+
+		  // Drive across field
+		  bot.navigation.resetAndDriveToPosition(0, 95, 0, 0.1);
+		  bot.navigation.resetAndDriveToPosition(-25, 0, 0, 0.35);
+		  bot.navigation.resetAndDriveToPosition(0, 10, 0, 0.2);
+		  bot.stickyPad.releaseRight();
+		  sleep(2000);
+
+		  bot.gotoStartArmPosition();
+		  sleep(2000);
+
+
+
+	   }
     }
 }

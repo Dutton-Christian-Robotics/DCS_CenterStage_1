2777ecd7a6e6e4797021ff479c56a4dd21cfc2d0
Matthew Roberts <thew.roberts@gmail.com>
2023-11-14 19:49:51 -0500

    Adjusted navigation and sensor method names

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/TurningTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/TurningTestOpMode.java
@@ -15,18 +15,18 @@ public class TurningTestOpMode extends LinearOpMode {
     public void runOpMode() {
 	   bot = new StickyBanditBot(hardwareMap, SBBConfiguration.class, telemetry);
 	   bot.opMode = this;
-	   telemetry.addData("Heading", bot.sensors.currentHeading());
+	   telemetry.addData("Heading", bot.sensors.getAngle());
 	   telemetry.update();
 
 	   waitForStart();
 
 //	   while (opModeIsActive()) {
 		bot.navigation.comeToRelativeHeading(45, 0.25, 1, 10000);
-		telemetry.addData("Heading", bot.sensors.currentHeading());
+		telemetry.addData("Heading", bot.sensors.getAngle());
 		telemetry.update();
 		sleep(5000);
 	   bot.navigation.comeToRelativeHeading(45, 0.25, 1, 10000);
-	   telemetry.addData("Heading", bot.sensors.currentHeading());
+	   telemetry.addData("Heading", bot.sensors.getAngle());
 	   telemetry.update();
 	   sleep(5000);
 //	   }

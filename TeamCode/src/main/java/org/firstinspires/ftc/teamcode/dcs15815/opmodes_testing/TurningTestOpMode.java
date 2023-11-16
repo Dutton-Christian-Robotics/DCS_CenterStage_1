9fa8a705eaa3aaad074fba852a78787e0e0abed7
Matthew Roberts <thew.roberts@gmail.com>
2023-11-16 12:13:18 -0500

    Testing changes, and disabling.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/TurningTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcs15815/opmodes_testing/TurningTestOpMode.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.dcs15815.StickyBanditBot.SBBConfiguration;
 import org.firstinspires.ftc.teamcode.dcs15815.StickyBanditBot.StickyBanditBot;
-
+@Disabled
 @TeleOp(name = "Turning Test", group = "Testing")
 public class TurningTestOpMode extends LinearOpMode {
     StickyBanditBot bot;
@@ -15,20 +15,44 @@ public class TurningTestOpMode extends LinearOpMode {
     public void runOpMode() {
 	   bot = new StickyBanditBot(hardwareMap, SBBConfiguration.class, telemetry);
 	   bot.opMode = this;
-	   telemetry.addData("Heading", bot.sensors.getAngle());
+	   telemetry.addData("Heading", bot.sensors.globalAngle);
 	   telemetry.update();
 
 	   waitForStart();
 
 //	   while (opModeIsActive()) {
-		bot.navigation.comeToRelativeHeading(45, 0.25, 1, 10000);
-		telemetry.addData("Heading", bot.sensors.getAngle());
-		telemetry.update();
-		sleep(5000);
-	   bot.navigation.comeToRelativeHeading(45, 0.25, 1, 10000);
-	   telemetry.addData("Heading", bot.sensors.getAngle());
+	   bot.navigation.comeToRelativeHeading(90, 1, 1, 10000);
+	   telemetry.addData("Heading", bot.sensors.globalAngle);
+	   telemetry.addData("Delta", bot.sensors.deltaAngle);
 	   telemetry.update();
 	   sleep(5000);
+
+//	   bot.navigation.resetAndDriveToPosition(0, 10, 0, .3);
+////	   bot.navigation.comeToRelativeHeading(45, 1, 1, 10000);
+//	   telemetry.addData("Heading", bot.sensors.globalAngle);
+//	   telemetry.addData("Delta", bot.sensors.deltaAngle);
+//	   telemetry.update();
+//	   sleep(5000);
+//
+//	   bot.navigation.resetAndDriveToPosition(0, -10, 0, .3);
+//	   telemetry.addData("Heading", bot.sensors.globalAngle);
+//	   telemetry.addData("Delta", bot.sensors.deltaAngle);
+//	   telemetry.update();
+//	   sleep(5000);
+//
+//
+//	   bot.navigation.comeToRelativeHeading(-90, 1, 1, 10000);
+//	   telemetry.addData("Heading", bot.sensors.globalAngle);
+//	   telemetry.addData("Delta", bot.sensors.deltaAngle);
+//	   telemetry.update();
+//	   sleep(5000);
+
+//	   bot.navigation.comeToRelativeHeading(-45, 1, 1, 10000);
+//	   telemetry.addData("Heading", bot.sensors.globalAngle);
+//	   telemetry.addData("Delta", bot.sensors.deltaAngle);
+//	   telemetry.update();
+//	   sleep(5000);
+
 //	   }
     }
 }
